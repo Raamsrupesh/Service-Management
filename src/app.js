5504963a8cd0c7c62d06c44a11986f6d36ffad99
Raamsrupesh <raamsrupesh@example.com>
2026-07-30 18:19:25 +0530

    chore: save workspace changes

--- src/app.js
+++ src/app.js
@@ -9,11 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/officer", officerRoutes);
-app.use("/api/worker", workerRoutes);
-app.use("/api/admin", adminRoutes);
+app.use("/api/auth/v1", authRoutes);
+app.use("/api/user/v1", userRoutes);
+app.use("/api/officer/v1", officerRoutes);
+app.use("/api/worker/v1", workerRoutes);
+app.use("/api/admin/v1", adminRoutes);
 
 app.get("/", async (req, res) => {
     res.status(200).send(`<h1 style="font-family: 'Segoe UI Semibold', 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-size: 2.5rem; font-weight: 600; line-height: 1.2; margin: 0 0 1rem 0;text-align:center;">🙏 🙏 Hari Hari 🙏 🙏</h1>`)
